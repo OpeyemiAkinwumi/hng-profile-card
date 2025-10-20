2881b8a72143a4e1a16f2ed8c84c0712a14a1cfb
OpeyemiAkinwumi <opeyemiakinwumi14@gmail.com>
2025-10-20 23:52:18 +0100

    Stage 1 task implemented - About and Contact Page

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const timeEl = document.querySelector(".time");
 
 const now = Date.now();
 
-console.log(now);
+console.log(now, timeEl);
 
 // const milli = now.getMilliseconds();
 
 // console.log(milli);
 
 timeEl.textContent = now;
+
+// Form Validation
